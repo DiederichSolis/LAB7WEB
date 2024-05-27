3c1bd567bb8dda867846c5e8ecd2b88e1508151a
Diederich Solis <sol2295@uvg.edu.gt>
2024-05-27 09:48:05 -0600

    feat: agregacion arreglo para mos botones

--- calculadora/app/Calculator.tsx
+++ calculadora/app/Calculator.tsx
@@ -6,7 +6,10 @@ export default function Calculator(){
 
 
     const Accionar = (value: string) => {
-        
+
     }
 
+  const botones = [
+    'C','+/-',' ','/','7', '8', '9', '*','4', '5', '6', '-','1', '2', '3', '+','0', '.', '', '='
+  ];
 }
