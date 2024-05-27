2c9a4b80efb6c7825af16d2f67e12c4cfa5129e5
Diederich Solis <sol2295@uvg.edu.gt>
2024-05-27 09:40:15 -0600

    feat: agregacion de constantes

---
+++ calculadora/app/Calculator.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Calculator(){
+    const [resultado, setResult]  = useState('');
+    const [data, setData] = useState ('');
+
+
+    const Accionar = (value: string) => {
+        
+    }
+
+}
