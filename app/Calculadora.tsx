d0e184ed58e4bc694e74c8875a95e9372d4517e2
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-27 18:16:47 -0600

    feat: arreglo y ajuste para mejor visualizacion de la calculadora

--- app/Calculadora.tsx
+++ app/Calculadora.tsx
@@ -67,35 +67,37 @@ export default function Calculator() {
 
     return (
         <main style={{ display: 'flex', minHeight: '100vh', flexDirection: 'column', alignItems: 'center', padding: '24px' }}>
-            <h1 style={{ fontSize: '2.5rem', color: 'rgb(255, 255, 255)', fontWeight: 'bold', marginBottom: '2.5rem' }}>Calculadora</h1>
-            <div style={{ backgroundColor: 'white', padding: '1.5rem', borderRadius: '4rem', boxShadow: '0 4px 6px rgba(0,0,0,0.1)' }}>
-                <input
-                    type="text"
-                    style={{ width: '100%', marginBottom: '0.5rem', fontSize: '2rem', borderBottom: '2px solid #ccc', outline: 'none' }}
-                    value={data}
-                    readOnly
-                />
-                <input
-                    type="text"
-                    style={{ width: '100%', fontSize: '2.5rem', fontWeight: 'bold', marginBottom: '1rem', outline: 'none' }}
-                    value={resultado}
-                    readOnly
-                />
-                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '0.5rem' }}>
-                    {botones.map((btn) => (
-                        <button
-                            key={btn}
-                            onClick={() => Accionar(btn)}
-                            style={{ fontSize: '1.5rem', color: 'rgb(255, 255, 255)', backgroundColor: 'rgb(2, 48, 71)', padding: ' 1rem', borderRadius: '8rem', cursor: 'pointer' }}
-                        >
-                            {btn}
-                        </button>
-                    ))}
-                </div>
+        <h1 style={{ fontSize: '2.5rem', color: 'rgb(255, 255, 255)', fontWeight: 'bold', marginBottom: '2.5rem' }}>Calculadora</h1>
+        <div style={{ backgroundColor: 'white', padding: '1.5rem',  borderRadius: '4rem',  boxShadow: '0 4px 6px rgba(0,0,0,0.1)', width: '100%', maxWidth: '400px',height: 'auto' 
+        }}>
+            <input
+                type="text"
+                style={{ width: '100%', marginBottom: '0.5rem', fontSize: '2rem', borderBottom: '2px solid #ccc', outline: 'none' }}
+                value={data}
+                readOnly
+            />
+            <input
+                type="text"
+                style={{ width: '100%', fontSize: '2.5rem', fontWeight: 'bold', marginBottom: '1rem', outline: 'none' }}
+                value={resultado}
+                readOnly
+            />
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '0.5rem' }}>
+                {botones.map((btn) => (
+                    <button
+                        key={btn}
+                        onClick={() => Accionar(btn)}
+                        style={{ fontSize: '1.5rem', color: 'rgb(255, 255, 255)', backgroundColor: 'rgb(2, 48, 71)', padding: '1rem', borderRadius: '8rem', cursor: 'pointer' }}
+                    >
+                        {btn}
+                    </button>
+                ))}
             </div>
-            <footer style={{ position: 'fixed', bottom: 0, background: 'rgba(0, 0, 0, 0.5)', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0%', height: '5vh', width: '100%', zIndex: 1000, }}>
-                <p>&copy; 2024 - Todos los derechos reservados Autor: Diederich Solis</p>
-            </footer>
-        </main>
-    );
-};
+        </div>
+        <footer style={{ position: 'fixed', bottom: 0, background: 'rgba(0, 0, 0, 0.5)', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0%', height: '5vh', width: '100%', zIndex: 1000 }}>
+            <p>&copy; 2024 - Todos los derechos reservados Autor: Diederich Solis</p>
+        </footer>
+    </main>
+    )
+                };
+    
